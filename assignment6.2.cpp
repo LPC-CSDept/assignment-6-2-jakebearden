95f737cc194b0d78bb0f2a39d14a3320d3fc0474
jake <jbearden364@gmail.com>
2022-10-11 08:54:15 -0700

    .

--- assignment6.2.cpp
+++ assignment6.2.cpp
@@ -3,7 +3,60 @@
 #include <iostream>
 using namespace std;
 
+void printout(int result);
+int nthpowern(int n);
+int nthpowerp(int n);
 int main()
 {
+    int i, n, result;
+    cout << "Enter input" << endl;
+    cin >> n;
+
+    if (n>0)
+    {
+        result = nthpowerp(n);
+        printout(result);
+    }
+
+    else if (n<0)
+    {
+        result = nthpowern(n);
+        printout(result);
+    }
+
+    else
+    {
+        result = 1;
+        printout(result);
+    }
     
+}
+
+int nthpowerp(int n)
+{
+    int i, result ;
+
+    for (i = 2; i < n; i++)
+    {
+        result *= 2;
+    }
+    return result;
+
+}
+
+int nthpowern(int n)
+{
+    int i, result;
+
+    for (i = 1/2; i < n; i++)
+    {
+        result = 1/2;
+        result *=  1/2;
+    }
+    return result;
+}
+
+void printout(int result)
+{
+    cout << result << endl;
 }
