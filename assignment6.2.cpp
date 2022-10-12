94bcd77330379169f9494ba3db3fa0ab3d52d408
jake <jbearden364@gmail.com>
2022-10-11 18:44:46 -0700

    .

--- assignment6.2.cpp
+++ assignment6.2.cpp
@@ -49,12 +49,12 @@ int nthpowern(int n)
 {
     int i, result;
 
-    result = 1;
+    result = 1/2;
 
-    while (n > 0)
+    while (n < 0)
     {
       result *= 1/2;
-      n -=1; 
+      n +=1; 
     }
     
     return result;
